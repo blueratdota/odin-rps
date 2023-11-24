92b555b3cca455b92837af53aec804d66d3a5113
blueratdota <jrafaelgocong@gmail.com>
2023-11-24 22:30:57 +0800

    playable ver

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Rock Paper Scissor Game
 # photos taken from
 www.pngegg.com
 # status
-11-22-23: done with game mechanics, need to put game ending code and stats to declare winner
+11-22-23: done with game mechanics, need to put game ending code and stats to declare winner  
+11-24-23: added game reset and game start. just need to polish more. finalize the css
 
 
--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const computerChoicePic = document.querySelector('#computer-choice')
 const roundCounter = document.querySelector('#round-counter')
 const yourScoreCounter = document.querySelector('#your-score')
 const compScoreCounter = document.querySelector('#computer-score')
+const yourTxtPokemon =  document.querySelector('#your-pokemon')
+const compTxtPokemon =  document.querySelector('#computer-pokemon')
+const battleResult =  document.querySelector('#battle-result')
+const resultText = document.querySelector('#bottom-text')
 
 const allPokemon = ['squirtle','charmander','bulbasaur']
 let yourPokemon = ''
@@ -13,19 +17,71 @@ let computerPokemon = ''
 let yourScore = 0
 let computerScore = 0
 let round = 0
+let gameStatus= 'play-mode'
+
+function resetAll(){
+    yourPokemon = ''
+    computerPokemon = ''
+    yourScore = 0
+    computerScore = 0
+    round = 0
+    yourTxtPokemon.textContent= ''
+    compTxtPokemon.textContent=''
+    yourScoreCounter.textContent = 0
+    compScoreCounter.textContent = 0
+    roundCounter.textContent = 0
+    yourChoicePic.src = "images/pokeball.png"
+    computerChoicePic.src = "images/pokeball.png"
+    battleResult.textContent = 'Good Luck, Trainer!'
+    battleResult.style.fontSize = '1.6rem';
+    battleResult.style.fontWeight = '100';
+}
 
 function win(){
     yourScore++
+    battleResult.textContent = 'You Won This Round!'
     console.log(`win! round=${round} you=${yourScore} | computer=${computerScore}`);
 }
 
 function lose(){
     computerScore++
+    battleResult.textContent = 'Computer Won This Round!'
     console.log(`lose! round=${round} you=${yourScore} | computer=${computerScore}`);
 }
 
 function draw(){
+    battleResult.textContent = "It's a Draw!"
     console.log(`draw! round=${round} you=${yourScore} | computer=${computerScore}`);
+}
+function playAgain(){
+    console.log('play again pressed');
+    const plyAgainBtn = document.querySelector('#play-button')
+    resultText.removeChild(plyAgainBtn)
+    resetAll()
+    gameStatus = 'play-mode'
+}
+
+function checkWinner(){
+    if (yourScore==6 || computerScore ==6){
+        if (yourScore==6){
+            console.log('you win')
+            battleResult.textContent = 'YOU WON THE BATTLE!'     
+        }
+        else {
+            battleResult.textContent = 'BETTER LUCK NEXT TIME, TRAINER!'
+            console.log('you lose');
+        }
+        battleResult.style.fontSize = '2.2rem';
+        battleResult.style.fontWeight = 'bold';
+        const plyAgnBtn = document.createElement('button');
+        plyAgnBtn.textContent = 'Play Again';
+        plyAgnBtn.setAttribute('id',"play-button")
+        resultText.appendChild(plyAgnBtn)
+        plyAgnBtn.addEventListener('click',function(){
+            playAgain()
+        })
+        gameStatus = 'wait-reset'
+    }
 }
 
 function computerChoosePokemon(){
@@ -79,12 +135,18 @@ function playRound(yourPokemon,computerPokemon){
     }
 }
 
+resetAll()
 const choices = [charmanderPic,bulbasaurPic,squirtlePic]
 choices.forEach((e)=>e.addEventListener('click',function(){
-    yourPokemon = e.alt
-    computerPokemon= computerChoosePokemon()
-    yourChoicePic.src = `images/${e.alt}.png`
-    computerChoicePic.src = `images/${computerPokemon}.png`
-    playRound(yourPokemon,computerPokemon)
-    update()
+    if(gameStatus=='play-mode'){
+        yourPokemon = e.alt
+        computerPokemon= computerChoosePokemon()
+        yourChoicePic.src = `images/${yourPokemon}.png`
+        computerChoicePic.src = `images/${computerPokemon}.png`
+        yourTxtPokemon.textContent = `${yourPokemon.charAt(0).toUpperCase()+yourPokemon.slice(1)}`
+        compTxtPokemon.textContent = `${computerPokemon.charAt(0).toUpperCase()+computerPokemon.slice(1)}`
+        playRound(yourPokemon,computerPokemon)
+        update()
+        checkWinner()
+    }
 }))
